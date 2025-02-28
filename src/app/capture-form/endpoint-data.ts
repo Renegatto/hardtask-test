16082ff2538f440907b1ac01f8b6460e8fd7d0a0
Renegatto <edittorr@mail.ru>
2025-02-28 09:18:40 +0300

    Implement publishTask

--- src/app/capture-form/endpoint-data.ts
+++ src/app/capture-form/endpoint-data.ts
@@ -1,16 +1,4 @@
 
-/*
-token=317ad1fc-e0a9-11ef-a978-0242ac120007
-title=<>
-description=<>
-tags=<>
-budget_from=1000
-budget_to=5000
-deadline=1
-reminds=3
-all_auto_responses=false
-rules=%7B%22budget_from%22%3A5000%2C%22budget_to%22%3A8000%2C%22deadline_days%22%3A5%2C%22qty_freelancers%22%3A1%7D'
-*/
 export type Task = {
   token: string; // uuid
   title: string;
@@ -23,6 +11,14 @@ export type Task = {
   all_auto_responses: boolean;
   rules: string;
 }
+export type PublishedTask = {
+  foo: number,
+}
+
+export type Result<E,A> =
+  | { success: true, value: A }
+  | { success: false, failure: E }
+
 const ENDPOINT_URL = "https://deadlinetaskbot.productlove.ru/api/v1/tasks/client/newhardtask"
 
 export const makeQuery = (toPublish: Task): string =>
@@ -33,3 +29,20 @@ export const makeQuery = (toPublish: Task): string =>
     .map(([queryParam, value]) => `${queryParam}=${value.toString()}`)
     .join('&')
   }`
+
+export const publishTask = async (task: Task): Promise<Result<Error,PublishedTask>> => {
+  const outcome = await fetch(makeQuery(task))
+  var result: Uint8Array[] = [] //new Uint8Array()
+  while (true) {
+    const chunk = await outcome.body?.getReader()?.read()
+    if (chunk?.value) result.push(chunk.value)
+    if (chunk?.done) break
+  }
+  const fullResponseBody = Buffer.from(Uint8Array.from(result))
+    .toString('utf8')
+  return parsePublishedTask(JSON.parse(fullResponseBody))
+}
+
+const parsePublishedTask = (task: unknown): Result<Error,PublishedTask> => {
+  throw new Error('Not implemented')
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className={styles["page"]}>
+      <main className={styles["main"]}>
         <CaptureForm/>
       </main>
-      <footer className={styles.footer}>
+      <footer className={styles["footer"]}>
       </footer>
     </div>
   );
