9adf3b74b38fcb01538fb00bf8209037f0005d01
Renegatto <edittorr@mail.ru>
2025-02-28 14:04:21 +0300

    Add example form fields

--- src/app/capture-form/task-form.tsx
+++ src/app/capture-form/task-form.tsx
@@ -1,11 +1,22 @@
 import { FC } from "react";
 import { Task } from "./endpoint-data";
+import { Checkbox, Form, Radio } from "antd";
 
 export type TaskFormProps = {
   onSubmit: (submitted: Task) => void,
 }
 
 export const TaskForm: FC<TaskFormProps> = () => {
-
-  return <div>A task form</div>
+  
+  return <Form>
+    <Form.Item label="Checkbox" name="disabled" valuePropName="checked">
+      <Checkbox>Checkbox</Checkbox>
+    </Form.Item>
+    <Form.Item label="Radio">
+      <Radio.Group>
+        <Radio value="apple"> Apple </Radio>
+        <Radio value="pear"> Pear </Radio>
+      </Radio.Group>
+    </Form.Item>
+  </Form>
 }
